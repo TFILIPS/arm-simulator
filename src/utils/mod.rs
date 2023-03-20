98a3922a707eba7acb73aa570be078bc6a39e634
Thomas Filips <thomas.filips@outlook.at>
2023-03-20 20:00:00 +0100

    Added trait for extracting bits

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,8 @@
+use std::ops::Bound;
+use std::ops::RangeBounds;
+
 #[derive(Debug, Clone, Copy)]
-pub enum Endian { Little, Big}
+pub enum Endian { Little, Big }
 
 pub fn slice_to_u16(slice: &[u8], encoding: &Endian) -> u16 {
     let buffer: [u8; 2] = slice
@@ -20,5 +23,34 @@ pub fn slice_to_u32(slice: &[u8], encoding: &Endian) -> u32 {
     match encoding {
         Endian::Little => u32::from_le_bytes(buffer),
         Endian::Big => u32::from_be_bytes(buffer),
+    }
+}
+
+pub trait BitAccess {
+   fn get_bit(&self, index: usize) -> bool;
+   fn cut_bits<T: RangeBounds<usize>>(&self, range: T) -> u32;
+}
+
+//todo error handling
+impl BitAccess for u32 {
+    fn get_bit(&self, index: usize) -> bool {
+        *self >> index & 1 == 1
+    }
+
+    fn cut_bits<T: RangeBounds<usize>>(&self, range: T) -> u32 {
+        let start = match range.start_bound() {
+            Bound::Included(&x) => x,
+            Bound::Excluded(&x) => x + 1,
+            Bound::Unbounded => 0,
+        };
+
+        let end = match range.end_bound() {
+            Bound::Included(&x) => x,
+            Bound::Excluded(&x) => x - 1,
+            Bound::Unbounded => 31,
+        };
+
+        let lshift: usize = 31 - end;
+        (*self << lshift) >> (start + lshift)
     }
 }
