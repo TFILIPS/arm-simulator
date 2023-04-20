5ce04aa770149491c04b9615378da63969e057dc
Thomas Filips <thomas.filips@outlook.at>
2023-04-21 00:08:01 +0200

    Added commandline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{process::exit};
+use std::{process::exit, env};
 
 use elf_loader::ELFFile;
 use simulated_cpu::{SimulatedCPU, names::RegNames};
@@ -8,32 +8,25 @@ mod simulated_cpu;
 mod utils;
 
 fn main() {
-    let mut cpu: SimulatedCPU = SimulatedCPU::new();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: ./{:} path_to_executable", args[0]);
+        exit(1);
+    }
+    let path: &str = &args[1];
+    let elf_file: ELFFile = ELFFile::load(path).unwrap_or_else(print_and_exit);
+    elf_file.check_header_values().unwrap_or_else(print_and_exit);
     
-    let elf_file: ELFFile = 
-        ELFFile::load("data/and").unwrap_or_else(print_error_exit);
-    elf_file.check_header_values().unwrap_or_else(print_error_exit);
-
-    elf_file.load_memory(cpu.get_memory()).unwrap_or_else(print_error_exit);
-    cpu.set_register(RegNames::PC, elf_file.get_entry_point() as i32 + 8);
-    cpu.set_register(RegNames::SP, 0x8000);
+    let mut cpu: SimulatedCPU = SimulatedCPU::new();
+    cpu.set_register(RegNames::PC, (elf_file.get_entry_point() + 8) as i32);
+    cpu.set_register(RegNames::SP, 0x4000);
     cpu.set_encoding(elf_file.get_encoding());
 
-    for _ in 0..16 {
-        cpu.step();
-    }
-
-    print_all_registers(&cpu);
+    elf_file.load_memory(cpu.get_memory()).unwrap_or_else(print_and_exit);
+    loop { cpu.step(); }
 }
 
-fn print_all_registers(cpu: &SimulatedCPU) {
-    for i in 0..16 {
-        println!("Register: {:?} Value: {:x?}", RegNames::from(i), cpu.get_register(RegNames::from(i)))
-    }
-}
-
-fn print_error_exit<T>(msg: String) -> T {
+fn print_and_exit<T>(msg: String) -> T {
     eprintln!("{msg}");
-    exit(-1);
+    exit(1);
 }
-
